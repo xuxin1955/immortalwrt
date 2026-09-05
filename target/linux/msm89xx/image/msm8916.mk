66fcf6e979f864bc71b43e33319a611470351dcc
xuxin1955 <x2874836915@qq.com>
2026-09-05 10:36:25 +0800

    update

--- target/linux/msm89xx/image/msm8916.mk
+++ target/linux/msm89xx/image/msm8916.mk
@@ -13,6 +13,7 @@ define Device/openstick-ufi001c
   DEVICE_MODEL := OpenStick UFI001C
   DEVICE_DTS := msm8916-thwc-ufi001c
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,ufi001c openstick-ufi001c
   DEVICE_PACKAGES := openstick_tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-ufi001c-firmware qcom-msm8916-openstick-ufi001c-wcnss-firmware qcom-msm8916-wcnss-openstick-ufi001c-nv
 endef
 TARGET_DEVICES += openstick-ufi001c
@@ -23,6 +24,7 @@ define Device/openstick-ufi001b
   DEVICE_MODEL := OpenStick UFI001B
   DEVICE_DTS := msm8916-thwc-ufi001b
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,ufi001b openstick-ufi001b
   DEVICE_PACKAGES := openstick_tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-ufi001b-firmware qcom-msm8916-openstick-ufi001b-wcnss-firmware qcom-msm8916-wcnss-openstick-ufi001b-nv
 endef
 TARGET_DEVICES += openstick-ufi001b
@@ -31,6 +33,7 @@ define Device/openstick-ufi103s
   $(Device/openstick-ufi001c)
   DEVICE_MODEL := OpenStick UFI103S
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,ufi001c openstick-ufi103s
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-ufi103s-firmware qcom-msm8916-openstick-ufi103s-wcnss-firmware qcom-msm8916-wcnss-openstick-ufi103s-nv
 endef
 TARGET_DEVICES += openstick-ufi103s
@@ -39,6 +42,7 @@ define Device/openstick-qrzl903
   $(Device/openstick-ufi001c)
   DEVICE_MODEL := OpenStick QRZL903
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,ufi001c openstick-qrzl903
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-qrzl903-firmware qcom-msm8916-openstick-qrzl903-wcnss-firmware qcom-msm8916-wcnss-openstick-qrzl903-nv
 endef
 TARGET_DEVICES += openstick-qrzl903
@@ -47,6 +51,7 @@ define Device/openstick-w001
   $(Device/openstick-ufi001c)
   DEVICE_MODEL := OpenStick W001
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,ufi001c openstick-w001
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-w001-firmware qcom-msm8916-openstick-w001-wcnss-firmware qcom-msm8916-wcnss-openstick-w001-nv
 endef
 TARGET_DEVICES += openstick-w001
@@ -55,6 +60,7 @@ define Device/openstick-ufi003
   $(Device/openstick-ufi001c)
   DEVICE_MODEL := OpenStick UFI003
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,ufi001c openstick-ufi003
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-ufi003-firmware qcom-msm8916-openstick-ufi003-wcnss-firmware qcom-msm8916-wcnss-openstick-ufi003-nv
 endef
 TARGET_DEVICES += openstick-ufi003
@@ -65,6 +71,7 @@ define Device/openstick-uz801
   DEVICE_MODEL := OpenStick UZ801
   DEVICE_DTS := msm8916-yiming-uz801v3
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := yiming,uz801-v3 openstick-uz801
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-uz801-firmware qcom-msm8916-openstick-uz801-wcnss-firmware qcom-msm8916-wcnss-openstick-uz801-nv
 endef
 TARGET_DEVICES += openstick-uz801
@@ -74,7 +81,8 @@ define Device/openstick-mf32
   DEVICE_VENDOR := XinXun
   DEVICE_MODEL := OpenStick MF32
   DEVICE_DTS := msm8916-ufi-mf32
-    DEVICE_DTS_DIR := ../dts
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,ufi-mf32 openstick-mf32
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-mf32-firmware qcom-msm8916-openstick-mf32-wcnss-firmware qcom-msm8916-wcnss-openstick-mf32-nv
 endef
 TARGET_DEVICES += openstick-mf32
@@ -85,6 +93,7 @@ define Device/openstick-mf601
   DEVICE_MODEL := OpenStick MF601
   DEVICE_DTS := msm8916-ufi-mf601
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := jsbsbxjxh66,MF601_MC_V05 openstick-mf601
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-mf601-firmware qcom-msm8916-openstick-mf601-wcnss-firmware qcom-msm8916-wcnss-openstick-mf601-nv
 endef
 TARGET_DEVICES += openstick-mf601
@@ -95,6 +104,7 @@ define Device/openstick-wf2
   DEVICE_MODEL := OpenStick WF2
   DEVICE_DTS := msm8916-xinxun-wf2
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := xinxun,wf2 openstick-wf2
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-wf2-firmware qcom-msm8916-openstick-wf2-wcnss-firmware qcom-msm8916-wcnss-openstick-wf2-nv kmod-gpio-button-hotplug kmod-button-hotplug kmod-input-gpio-keys kmod-input-gpio-keys-polled
 endef
 TARGET_DEVICES += openstick-wf2
@@ -105,6 +115,7 @@ define Device/openstick-jz02v10
   DEVICE_MODEL := OpenStick JZ02V10
   DEVICE_DTS := msm8916-thwc-jz02v10
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := thwc,jz02v10 openstick-jz02v10
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-jz02v10-firmware qcom-msm8916-openstick-jz02v10-wcnss-firmware qcom-msm8916-wcnss-openstick-jz02v10-nv
 endef
 TARGET_DEVICES += openstick-jz02v10
@@ -115,6 +126,7 @@ define Device/openstick-sp970v11
   DEVICE_MODEL := OpenStick SP970V11
   DEVICE_DTS := msm8916-gexing-sp970
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := gexing,sp970 openstick-sp970v11
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-sp970v11-firmware qcom-msm8916-openstick-sp970v11-wcnss-firmware qcom-msm8916-wcnss-openstick-sp970v11-nv
 endef
 TARGET_DEVICES += openstick-sp970v11
@@ -125,6 +137,7 @@ define Device/openstick-sp970v10
   DEVICE_MODEL := OpenStick SP970V10
   DEVICE_DTS := msm8916-gexing-sp970
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := gexing,sp970 openstick-sp970v10
   DEVICE_PACKAGES := openstick-tweaks wpad-basic-wolfssl qcom-msm8916-modem-openstick-sp970v10-firmware qcom-msm8916-openstick-sp970v10-wcnss-firmware qcom-msm8916-wcnss-openstick-sp970v10-nv
 endef
 TARGET_DEVICES += openstick-sp970v10
